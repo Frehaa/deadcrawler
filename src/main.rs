ca478b35600c5a814b0599cd1c87ae91bd54c64f
Frederik Haagensen <haag@itu.dk>
2022-11-01 16:39:50 +0100

    Add url crate and try it out

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use http::Uri;
 use hyper::{client::Client, body::HttpBody};
 use hyper_tls::HttpsConnector;
+use url::Url;
 
 use std::env;
 use std::collections::HashSet;
 
 #[tokio::main]
 async fn get_data(uri: Uri) -> Result<String, Box<dyn std::error::Error + Send + Sync>>{
-    let client = Client::new();
-
     let connector = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(connector);
 
@@ -21,7 +20,7 @@ async fn get_data(uri: Uri) -> Result<String, Box<dyn std::error::Error + Send +
     println!("{:?}", response);
 
     println!("Status {status} - version {version:?} - extensions {extensions:?}");
-    
+
     for key in headers.keys() {
         println!("headers[{key}] = {:?}", headers.get(key).unwrap());
     }
@@ -34,11 +33,58 @@ async fn get_data(uri: Uri) -> Result<String, Box<dyn std::error::Error + Send +
     Ok(String::from_utf8(v.concat())?)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+// TODO: Check for subdomains myself or not? Do I want to crawl over github if I find a link to it? Maybe I should instead make a separate crawl for subdomains. Or maybe just make it toggleable
+fn main() -> () { //Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    // let base = "https://www.itu.dk/".parse::<Url>().unwrap();
+
+    // let part = "#a";
+
+    // let join = base.join(&part);
+    // println!("{:?}", join);
+
+    // let r = join.unwrap().host().unwrap().eq(&base.host().unwrap());
+    // println!("{r}");
+    // join.unwrap().
+    
     // let mut args = env::args();
-    // let address = args.nth(1).unwrap();
-    // let uri = address.parse()?;
+    // let address = args.nth(1);
+    // if address.is_none() {
+    //     println!("No address given.");
+    //     return;
+    // }
+    // let address = address.unwrap();
+
+    
+    // let url = address.parse::<Url>();
+
+    // if url.is_err() {
+    //     println!("Could not parse as Url: {}", address);
+    //     return;
+    // }
+
+    // let url = url.unwrap();
+
+    // println!("{}", url.scheme());
+
+    // let scheme = url.scheme();
+    // if scheme.is_none() {
+    //     println!("Invalid Uri scheme: Not http or https");
+    //     return;
+    // }
+
+    // let scheme = scheme.unwrap();
+
+    // println!("{address}");
+    // println!("{url:?}");
+    // println!("{}", uri.path());
+    // println!("{:?}", &uri.path_and_query());
+    // println!("{:?}", uri.host());
+    // let parts = &uri.into_parts();
+    // println!("{:?}", parts);
     // let full_body = get_data(uri);
+
+
+    // return;
 
     let full_body = include_str!("itu.dk.txt");
 
@@ -56,6 +102,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
                 state = 1;
                 curr_idx = i;
             },
+            ('>', 0) => panic!("Error at character {i}: Unexpected '>'"),// No end of tag before start of tag
             ('>', 8) => {
                 panic!("Didn't expect end of tag in state 8");
             },
@@ -91,10 +138,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
             (_, 8) => {
                 href.push(c);
             },
-            _ => {
-                // No end of tag before start of tag
-                assert!(!(state == 0 && c == '>'), "Error at character {i}: Unexpected '>'")
-            }
+            _ => () 
         }
     }
     
@@ -128,5 +172,4 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
     // let r = response.poll();
 
 
-    Ok(())
 }

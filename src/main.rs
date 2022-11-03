acba7b300d1de8a5459e9dcbd7007e1b5628cace
Frederik Haagensen <haag@itu.dk>
2022-11-03 16:18:55 +0100

    Add tests and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use http::Uri;
+use http::uri::Scheme;
+use http::{Uri, Error, StatusCode};
 use hyper::{client::Client, body::HttpBody};
 use hyper_tls::HttpsConnector;
 use url::Url;
@@ -6,77 +7,159 @@ use url::Url;
 use std::env;
 use std::collections::HashSet;
 
+#[derive(Debug)]
+enum ResponseStatusCode {
+    NOT_FOUND,
+    OK,
+    OTHER
+}
+
+#[derive(Debug)]
+struct ResponseData {
+    statusCode : ResponseStatusCode,
+    body : Option<String>
+}
+
+#[derive(Debug)]
+struct ATag {
+    start_idx: usize,
+    id: Option<String>,
+    href: String
+}
+
 #[tokio::main]
-async fn get_data(uri: Uri) -> Result<String, Box<dyn std::error::Error + Send + Sync>>{
+async fn get_data(uri: Uri) -> Result<ResponseData, Box<dyn std::error::Error + Send + Sync>>{
     let connector = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(connector);
 
     let mut response = client.get(uri).await?;
 
     let status = response.status();
-    let version = response.version();
-    let headers = response.headers();
-    let extensions = response.extensions();
-    println!("{:?}", response);
 
-    println!("Status {status} - version {version:?} - extensions {extensions:?}");
+    match status {
+        StatusCode::NOT_FOUND => {
+            Ok(ResponseData { statusCode: ResponseStatusCode::NOT_FOUND, body: None })
+        },
+        StatusCode::OK => {
+            let mut v = Vec::new();
+            while let Some(chunk) = response.body_mut().data().await {
+                v.push(chunk?);
+            }
+            Ok(ResponseData { 
+                statusCode: ResponseStatusCode::OK, 
+                body: Some(String::from_utf8(v.concat())?) 
+            })
+        },
+        _ => {
+            Ok(ResponseData { statusCode: ResponseStatusCode::OTHER, body: None })
+        }
 
-    for key in headers.keys() {
-        println!("headers[{key}] = {:?}", headers.get(key).unwrap());
+
     }
 
-    let mut v = Vec::new();
-    while let Some(chunk) = response.body_mut().data().await {
-        v.push(chunk?);
-    }
+    // let version = response.version();
+    // let headers = response.headers();
+    // let extensions = response.extensions();
+    // println!("{:?}", response);
+    // println!("Status {status} - version {version:?} - extensions {extensions:?}");
 
-    Ok(String::from_utf8(v.concat())?)
+    // for key in headers.keys() {
+    //     println!("headers[{key}] = {:?}", headers.get(key).unwrap());
+    // }
+
 }
 
-// TODO: Check for subdomains myself or not? Do I want to crawl over github if I find a link to it? Maybe I should instead make a separate crawl for subdomains. Or maybe just make it toggleable
-fn main() -> () { //Result<(), Box<dyn std::error::Error + Send + Sync>>{
+fn parse_to_uri(address : &str) -> Result<Uri, &str> {
+    let uri = address.parse::<Uri>();
+    if uri.is_err() {
+        return Err("Could not parse as Uri");
+    }
+    let uri = unsafe { uri.unwrap_unchecked() };
+
+    let scheme = uri.scheme().filter(|&s| Scheme::HTTP.eq(s) || Scheme::HTTPS.eq(s));
+    if scheme.is_none() {
+        return Err("Invalid Uri scheme: Not http or https");
+    }
+
+    Ok(uri)
+}
+
+fn url_to_uri(url: Url) -> Uri {
+    todo!()
+}
+
+// Figure out if the new address is relative and if so, join it to the base, otherwise use it
+fn consolidate_uri(address : &str, base : &Url) -> Uri {
+    // How to join url with relative paths? 
     // let base = "https://www.itu.dk/".parse::<Url>().unwrap();
 
     // let part = "#a";
+    let url = address.parse::<Url>();
+    if let Ok(r) = url { // This is not 
+        return url_to_uri(r);
+    }
 
-    // let join = base.join(&part);
-    // println!("{:?}", join);
+    let join = base.join(&address);
+    println!("{:?}", join);
 
     // let r = join.unwrap().host().unwrap().eq(&base.host().unwrap());
     // println!("{r}");
     // join.unwrap().
-    
-    // let mut args = env::args();
-    // let address = args.nth(1);
-    // if address.is_none() {
-    //     println!("No address given.");
-    //     return;
-    // }
-    // let address = address.unwrap();
 
-    
-    // let url = address.parse::<Url>();
+    Uri::default()
+}
 
-    // if url.is_err() {
-    //     println!("Could not parse as Url: {}", address);
-    //     return;
-    // }
+// TODO: Check for subdomains myself or not? Do I want to crawl over github if I find a link to it? Maybe I should instead make a separate crawl for subdomains. Or maybe just make it toggleable
+fn main() -> () { //Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    let result = parse_to_uri("https://github.com");
 
-    // let url = url.unwrap();
+    let uri = result.unwrap();
 
-    // println!("{}", url.scheme());
+    println!("{:?}", uri.scheme_str());
+    println!("{:?}", uri.path());
+    println!("{:?}", uri.query());
+    println!("{:?}", uri.host());
 
-    // let scheme = url.scheme();
-    // if scheme.is_none() {
-    //     println!("Invalid Uri scheme: Not http or https");
-    //     return;
-    // }
 
-    // let scheme = scheme.unwrap();
+
+    return;
+
+    let mut args = env::args();
+
+    let address = args.nth(1);
+    if address.is_none() {
+        println!("Error: No address given.");
+        return;
+    }
+    let address = unsafe { address.unwrap_unchecked() };
+
+    let uri = parse_to_uri(&address);
+    if let Err(e) = uri {
+        println!("Error: {}", e);
+        return;
+    }
+
+    let data = get_data(unsafe { uri.unwrap_unchecked() });
+    if let Err(e) = data {
+        println!("Something went wrong {}", e);
+        return;
+    }
+    let data = unsafe { data.unwrap_unchecked() };
+
+    match data.statusCode {
+        ResponseStatusCode::NOT_FOUND => {
+
+        },
+        ResponseStatusCode::OK => {
+
+        },
+        ResponseStatusCode::OTHER => {
+
+        }
+    }
 
     // println!("{address}");
     // println!("{url:?}");
-    // println!("{}", uri.path());
     // println!("{:?}", &uri.path_and_query());
     // println!("{:?}", uri.host());
     // let parts = &uri.into_parts();
@@ -90,28 +173,37 @@ fn main() -> () { //Result<(), Box<dyn std::error::Error + Send + Sync>>{
 
     let mut state = 0;
 
-    let mut links = HashSet::new();
-    let mut href = Vec::new();
-
+    let mut atags = Vec::new();
+    let mut hrefbuilder = Vec::new();
     let mut a_tags_idx = Vec::new();
 
-    let mut curr_idx = 0;
+    let mut current_href = "".to_string();
+    let mut tag_start_idx = 0;
     for (i, c) in full_body.chars().enumerate() {
         match (c, state) {
             ('<', 0) => { // Start of tag
                 state = 1;
-                curr_idx = i;
+                tag_start_idx = i;
             },
             ('>', 0) => panic!("Error at character {i}: Unexpected '>'"),// No end of tag before start of tag
             ('>', 8) => {
                 panic!("Didn't expect end of tag in state 8");
             },
             ('>', _) => { // End of tag
+                if state != -1 {
+                    let atag = ATag { 
+                        start_idx: tag_start_idx, 
+                        id: None, // TODO: Include parsing of id
+                        href: current_href
+                    };
+                    current_href = "".to_string();
+                    atags.push(atag);
+                }
                 state = 0;
             },
             ('a', 1) => {
                 state = 2;
-                a_tags_idx.push(curr_idx);
+                a_tags_idx.push(tag_start_idx);
             },
             (_, 1) => { // Not a start 'a' tag
                 state = -1;
@@ -131,22 +223,23 @@ fn main() -> () { //Result<(), Box<dyn std::error::Error + Send + Sync>>{
             },
             (_, 7) => state = 2,
             ('"', 8) => { // Exit href src
-                links.insert(String::from_iter(href.iter()));
-                href = Vec::new();
+                current_href = String::from_iter(hrefbuilder.iter());
+                hrefbuilder = Vec::new();
                 state = 2;
             },
             (_, 8) => {
-                href.push(c);
+                hrefbuilder.push(c);
             },
             _ => () 
         }
     }
-    
 
-    println!("Number of a's {} - number of unique links {}", &a_tags_idx.len(), &links.len());
-    for link in links.iter() {
-        println!("{}", link);
+    println!("Number of a's {}", &atags.len());
+    for atag in atags.iter() {
+        println!("{:?}", atag);
     }
+
+
     // for idx in a_tags_idx.iter() {
     //     print!("{idx} ");
     //     // println!("{}", String::from_iter(tag.iter()));
@@ -172,4 +265,69 @@ fn main() -> () { //Result<(), Box<dyn std::error::Error + Send + Sync>>{
     // let r = response.poll();
 
 
+}
+
+#[cfg(test)]
+mod parse_to_uri_tests {
+    use http::{Uri, uri::Scheme};
+
+    use crate::parse_to_uri;
+
+    #[test]
+    fn https_and_authority() {
+        let result = parse_to_uri("https://github.com/");
+
+        assert!(result.is_ok());
+
+        let uri = result.unwrap();
+
+        assert_eq!(uri.scheme_str(), Some("https"));
+        assert_eq!(uri.path(), "/");
+        assert_eq!(uri.query(), None);
+        assert_eq!(uri.host(), Some("github.com"));
+    }
+
+    #[test]
+    fn http_and_authority_with_subdomain() {
+        let result = parse_to_uri("http://www.github.com/");
+
+        assert!(result.is_ok());
+
+        let uri = result.unwrap();
+
+        assert_eq!(uri.scheme_str(), Some("http"));
+        assert_eq!(uri.path(), "/");
+        assert_eq!(uri.query(), None);
+        assert_eq!(uri.host(), Some("www.github.com"));
+    }
+
+    #[test]
+    fn no_final_slash_parses_to_slash_in_path() {
+        let result = parse_to_uri("https://github.com");
+
+        assert!(result.is_ok());
+
+        let uri = result.unwrap();
+
+        assert_eq!(uri.scheme_str(), Some("https"));
+        assert_eq!(uri.path(), "/");
+        assert_eq!(uri.query(), None);
+        assert_eq!(uri.host(), Some("github.com"));
+    }
+
+    #[test]
+    fn non_http_scheme_is_error() {
+        let result = parse_to_uri("mailto:frehaa@github.com");
+        assert!(result.is_err());
+    }
+
+}
+
+#[cfg(test)]
+mod moretests {
+    #[test]
+    fn it_also_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
